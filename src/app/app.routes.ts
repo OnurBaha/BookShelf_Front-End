0d9b6ea414ba63638d1508beb16759256292eb27
OnurBaha <koconurbaha@gmail.com>
2024-12-03 21:12:53 +0300

    Page designs added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { BookListComponent } from './pages/book/book-list/book-list.component';
+import { AddBookComponent } from './pages/book/add-book/add-book.component';
+import { FavoriteBooksComponent } from './pages/book/favorite-books/favorite-books.component';
+import { ReadingPlanComponent } from './pages/reading-plan/reading-plan/reading-plan.component';
+import { BookDetailsComponent } from './pages/book/book-details/book-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/books', pathMatch: 'full' },
+    { path: 'books', component: BookListComponent },
+    { path: 'add-book', component: AddBookComponent },
+    { path: 'favorites', component: FavoriteBooksComponent },
+    { path: 'reading-plan', component: ReadingPlanComponent },
+    { path: 'books/:id', component: BookDetailsComponent },
+];
